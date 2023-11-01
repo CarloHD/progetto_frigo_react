fd20970842d6e4f86d2a4c7aac57ddfb2a350488
CarloHD <carloborrometi@gmail.com>
2023-11-01 21:27:34 +0100

    fix warning

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import { EditPage } from './pages/EditPage'
 import { RootPage } from './pages/RootPage'
 
 import { FrigoProvider } from './store/frigo-context'
-
-import CSS from './App.module.css'
 import { TestPage } from './pages/TestPage'
 
 const route = createBrowserRouter([
